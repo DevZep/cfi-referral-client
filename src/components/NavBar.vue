e2cab3f383c57f89602aa67d2f94734977761cee
Darren Jensen <darren.jensen@gmail.com>
2020-08-16 13:07:00 +0700

    added a navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <v-toolbar>
+      <v-toolbar-title class="grey--text">
+        <span class="font-weight-light">CFI</span>
+        <span>Referral</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text color="grey">
+        <span>Sign Out</span>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class NavBar extends Vue {}
+</script>
