29cfeb83e2b7280ec34290afe5720b75e1ea50d7
Darren Jensen <darren.jensen@gmail.com>
2020-08-20 17:33:33 +0700

    refactoring for vuex store logic separation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,120 +1,21 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import { Auth } from 'aws-amplify'
+import Vuex, { ModuleTree } from 'vuex'
+
+import Auth, { AuthState } from './modules/auth'
+// import accounts from "./modules/accounts";
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    user: null,
-    token: localStorage.getItem('user-token') || null,
-    status: '',
-    newUserEmail: '',
-    statusMessage: ''
-  },
-  mutations: {
-    // Synchronous
-    setUser (state, user) {
-      state.user = user
-    },
-    setNewUserEmail (state, newUserEmail) {
-      state.newUserEmail = newUserEmail
-    },
-    authRequest (state) {
-      state.status = 'loading'
-    },
-    authSuccess (state, token) {
-      state.status = 'success'
-      state.token = token
-    },
-    authError (state) {
-      state.status = 'error'
-      state.token = null
-    },
-    clearSession (state) {
-      state.status = ''
-      state.user = null
-      state.token = null
-    },
-    setStatusMessage (state, message) {
-      state.statusMessage = message
-    }
-  },
-  actions: {
-    // Asynchronous
-    async login ({ commit }, { email, password }) {
-      commit('authRequest')
-      try {
-        const user = await Auth.signIn(email, password)
-        const token = user.signInUserSession.accessToken.jwtToken
-        commit('setUser', user.attributes)
-        commit('authSuccess', token)
-        localStorage.setItem('user-token', token)
-      } catch (e) {
-        localStorage.removeItem('user-token')
-        commit('authError')
-      }
-    },
-    async signOut ({ commit }) {
-      try {
-        commit('clearSession')
-        localStorage.removeItem('user-token')
-        await Auth.signOut()
-      } catch (error) {
-        // console.log('error signing out: ', error)
-      }
-    },
-    // currentSession action handles when user refreshes the app
-    async currentSession ({ commit, dispatch }) {
-      commit('authRequest')
-      try {
-        const session = await Auth.currentSession()
-        const user = await Auth.currentUserInfo()
-        commit('authSuccess', session.getIdToken().getJwtToken())
-        commit('setUser', user.attributes)
-      } catch (e) {
-        // AWS Session error so signOut!
-        dispatch('signOut')
-      }
-    },
-    async signUp ({ commit, dispatch }, { email, password }) {
-      try {
-        commit('setNewUserEmail', email)
-        const newUser = await Auth.signUp(email, password)
-      } catch (e) {
-        if (e.code === 'UsernameExistsException') {
-          commit('setStatusMessage', 'This email already exists. Sending another code now.')
-          dispatch('resendCode')
-        }
-      }
-    },
-    async resendCode ({ state }) {
-      try {
-        await Auth.resendSignUp(state.newUserEmail)
-      } catch (e) {
-        console.log('Error in resendCode: ', e)
-      }
-    },
-    async confirmCode ({ commit, state }, code) {
-      try {
-        const resp = await Auth.confirmSignUp(state.newUserEmail, code)
-        commit('setStatusMessage', 'Perfect! Your code was correct. Now you can login!')
-      } catch (e) {
-        if (e.code === 'NotAuthorizedException') {
-          commit('setStatusMessage', 'You are probably already confirmed. Try logging in!')
-        }
-        commit('setStatusMessage', e.message)
-        console.log('Error in confirmCode: ', e)
-      }
-    }
-  },
-  modules: {
-  },
-  getters: {
-    getUser: state => state.user,
-    isAuthenticated: state => !!state.token,
-    authStatus: state => state.status,
-    isNewUser: state => state.newUserEmail,
-    statusMessage: state => state.statusMessage
-  }
-})
+const debug = process.env.NODE_ENV !== 'production'
+
+export interface RootState {
+  Auth: AuthState;
+}
+
+const modules: ModuleTree<RootState> = {
+  Auth
+}
+
+const store = new Vuex.Store({ modules })
+
+export default store
