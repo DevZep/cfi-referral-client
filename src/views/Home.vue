57460de7caba3825a26f7085d28a2f06c69b4378
Darren Jensen <darren.jensen@gmail.com>
2020-08-24 10:37:05 +0700

    demo purpose

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,7 @@
       </div>
       <div v-if="isAuthenticated && authStatus !== 'loading'">
         <p id='welcomeMessage'>Welcome {{ user.email }}</p>
+        <h1>For Demo Purpose</h1>
         <router-link to='referralForm'>Create New Referral</router-link>
       </div>
     </div>
