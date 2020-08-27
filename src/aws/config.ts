dd871c9147b441c898ba96f6e4b3d8de9172cff0
Darren Jensen <darren.jensen@gmail.com>
2020-08-27 17:50:22 +0700

    added staging environment config

--- src/aws/config.ts
+++ src/aws/config.ts
@@ -15,6 +15,23 @@ const dev = {
   }
 }
 
+const staging = {
+  s3: {
+    REGION: 'ap-southeast-1',
+    BUCKET: 'cfi-referral-api-staging-attachments-1cohl5t7ziug3'
+  },
+  apiGateway: {
+    REGION: 'ap-southeast-1',
+    URL: 'https://t39rl5hs62.execute-api.ap-southeast-1.amazonaws.com/staging'
+  },
+  cognito: {
+    REGION: 'ap-southeast-1',
+    USER_POOL_ID: 'ap-southeast-1_aqZmqBFm5',
+    APP_CLIENT_ID: '6sq09m16d2liq90rpii8gv2j8',
+    IDENTITY_POOL_ID: 'ap-southeast-1:8c6726f0-4027-4870-9196-3d07e042cb81'
+  }
+}
+
 const prod = {
   s3: {
     REGION: 'ap-southeast-1',
@@ -32,9 +49,12 @@ const prod = {
   }
 }
 
-const config = process.env.VUE_APP_STAGE === 'prod'
-  ? prod
-  : dev
+// default config to dev settings
+let config = dev
+
+// unless set by the VUE_APP_STAGE env variable
+if (process.env.VUE_APP_STAGE === 'prod') config = prod
+if (process.env.VUE_APP_STAGE === 'staging') config = staging
 
 export default {
   // Add common config values here
